045a0c0346ff9253121f68208de80dec892b42e9
sunwe <s2564099@ed.ac.uk>
2025-08-21 00:17:02 +0800

    新增员工分页查询功能

--- sky-server/src/main/java/com/sky/service/EmployeeService.java
+++ sky-server/src/main/java/com/sky/service/EmployeeService.java
@@ -2,9 +2,21 @@ package com.sky.service;
 
 import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
+import com.sky.result.PageResult;
 
+/**
+ *
+ */
 public interface EmployeeService {
+
+    /**
+     * PageQuery
+     * @param employeePageQueryDTO
+     * @return
+     */
+    PageResult pageQuery(EmployeePageQueryDTO employeePageQueryDTO);
 
     /**
      * 员工登录
